6bfab2ad7d67ca4fdd8beb4d22456d4c6de69a15
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-10 15:22:26 +0100

    complete fourth snack

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,12 +1,61 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
 // Dichiara la funzione qui.
 
+/* function vowelsString(word) {
+    let foundVowels = []
+    for (i = 0; i < word.length; i++) {
+        const char = word[i]
+        for (j = 0; j < vowelsArray.length; j++) {                     ////with normal sintax function and with two for cicle
+            const vowels = vowelsArray[j]
+            if (char === vowels) {
+                foundVowels.push(char)
+            }
+        }
+    }
+    if (foundVowels.length===0){
+        console.log("there aren't vowels in this word");
+        
+    }
+    return foundVowels
+} */
+
+/* function vowelsString(word) {
+    let foundVowels = []
+    for (const char of word) {
+        for (const vowel of vowelsArray) {                              //with normal sintax function and with two for..of            
+            if (char === vowel) {                                          DOCUMENTATOMI SU INTERNET HO LETTO CHE C'E' ANCHE
+                foundVowels.push(char)                                      QUESTA ALTERNATIVA
+            }
+        }
+
+    }
+    return foundVowels
+    
+} */
+const vowelsString = (word) => {
+    let foundVowels = []
+    for (i = 0; i < word.length; i++) {
+        const char = word[i]
+        for (j = 0; j < vowelsArray.length; j++) {                       //with sintax arrow function
+            const vowels = vowelsArray[j]
+            if (char === vowels) {
+                foundVowels.push(char)
+            }
+        }
+    }
+    if (foundVowels.length === 0) {
+        console.log("there aren't vowels in this word");
+
+    }
+    return foundVowels
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(vowelsString(word));
 
 
 
