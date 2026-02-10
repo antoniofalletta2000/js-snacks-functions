52d848f8214f6e1725a97271ea0a7561e5795784
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-10 13:34:56 +0100

    add arrow function in first snack

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,14 +4,19 @@ const userName = 'Mario';
 const hi = "Ciao"
 
 // Dichiara la funzione qui.
-function concatenation (){
-    const hi_name = (hi +" "+userName)
+/*function concatenation (){
+    const hi_name = (hi +" "+userName)       //with normal sintax function
+    return hi_name
+}*/
+const concatenation=()=>{
+    const hi_name = (hi +" "+userName)      //with sintax arrow function                       
     return hi_name
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(concatenation());
-
+/* console.log(concatenation()); */
+    console.log(concatenation());
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
-console.log(concatenation("Mario"));
+/* console.log(concatenation("Mario")); */
+    console.log(concatenation("Mario"));
