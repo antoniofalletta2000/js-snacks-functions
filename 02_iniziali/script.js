09edfe5ca4d4c10d351cae3071f84739c7a10a6b
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-10 13:53:35 +0100

    complete second snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/* function firstChar (names){
+    const newArray = []
+    for (let i=0; i<names.length;i++){               //with normal sintax function
+        newArray.push(names[i].charAt(0))
+    }
+    return newArray
+} */
 
-
+const firstChar = (names) =>{
+     const newArray = []
+    for (let i=0; i<names.length;i++){             //with sintax arrow function
+        newArray.push(names[i].charAt(0))
+    }
+    return newArray
+} 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(firstChar (names));
 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
